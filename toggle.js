51517802b48cd7f1c446b2bc2ff66c3ed261e2ea
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-12-27 21:40:31 +0100

    Replicate Precious Pharmacy UI

---
+++ toggle.js
@@ -0,0 +1,53 @@
+function openSideMenu(){
+  document.getElementById('primaryNav').style.height = '180px';
+  document.getElementById('primaryNav').classList.toggle("open");
+  document.getElementById('hamburgerBtn').classList.toggle("open1");
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+/*
+function toggleMenu() {
+  document.getElementById("primaryNav").classList.toggle("open");
+}
+let x = document.getElementById("hamburger");
+x.onclick = toggleMenu;
+*/
